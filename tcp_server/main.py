17edeb3d6f29d0382ac2a2fbc61722c8d6f072e5
jhr25864 <53107945+jhr25864@users.noreply.github.com>
2023-07-17 11:30:00 +0800

    jhr-ewew
    vnvnv

--- tcp_server/main.py
+++ tcp_server/main.py
@@ -10,8 +10,9 @@ class Node:
     def __init__(self):
         self.Name = None    # 小飞机序号
         self.Thr = None     # 套接字连接对象
-        self.Name = None    # 小飞机序号
-        self.Thr = None     # 套接字连接对象
+
+        #jhr
+
 
 
 class TcpServer:
